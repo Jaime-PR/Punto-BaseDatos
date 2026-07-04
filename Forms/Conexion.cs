6f23621b6d46a8bd4d82ccf37a92ee21fc32413a
Jaime Alfredo Perez Rivera <jaimealfredo987@gmail.com>
2026-07-04 16:22:52 -0600

    Actualiza configuraciones y añade clase de conexión
    Se han añadido redirecciones de enlace en `App.config` y se ha corregido la cadena de conexión para `InventarioDB`. En `Punto.csproj`, se han incorporado nuevas referencias a bibliotecas como `BouncyCastle.Cryptography`, `Google.Protobuf` y `MySql.Data`, además de un nuevo archivo `packages.config` que lista las dependencias. También se ha creado la clase `Conexion` en `Conexion.cs` para gestionar la conexión a la base de datos MySQL.

---
+++ Forms/Conexion.cs
@@ -0,0 +1,28 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Punto.Forms
+{
+    internal class Conexion
+    {
+        private readonly string Cadena;
+
+        public Conexion()
+        {
+            Cadena = "Server=127.0.0.1; Database=puntodb; Uid=root; Pwd=; Port=3307";
+        }
+
+        public MySqlConnection GetConnection()
+        {
+            MySqlConnection conexion = new MySqlConnection(Cadena);
+            conexion.Open();
+            return conexion;
+        }
+
+    }
+}
