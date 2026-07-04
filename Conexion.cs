d2c4a80a62078ddc2b68dcc332832d9d7549a3b3
Jaime Alfredo Perez Rivera <jaimealfredo987@gmail.com>
2026-07-04 16:31:33 -0600

    Actualizacion

--- Conexion.cs
+++ Conexion.cs
@@ -1,25 +1,24 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
 using System.Collections.Generic;
-using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Punto.Forms
+namespace Punto
 {
     internal class Conexion
     {
-        private readonly string Cadena;
+        private readonly string cadena;
 
         public Conexion()
         {
-            Cadena = "Server=127.0.0.1; Database=puntodb; Uid=root; Pwd=; Port=3307";
+            cadena = "Server=127.0.0.1; Database=puntodb; Uid=root; Pwd=; Port=3307";
         }
 
         public MySqlConnection GetConnection()
         {
-            MySqlConnection conexion = new MySqlConnection(Cadena);
+            MySqlConnection conexion = new MySqlConnection(cadena);
             conexion.Open();
             return conexion;
         }
