abe4e6b982d087da994870bb16816adfb821cdd7
Jaime Alfredo Perez Rivera <jaimealfredo987@gmail.com>
2026-07-04 21:36:27 -0600

    Mejoras en el formulario de productos
    Se han añadido y modificado controles en `frmProductos.Designer.cs`, incluyendo campos de entrada y botones, así como eventos para la búsqueda y selección de productos.
    
    En `frmProductos.cs`, se ha implementado la lógica para cargar categorías y manejar la búsqueda, junto con métodos para limpiar campos y cargar datos desde la base de datos.
    
    Se ha actualizado `Punto.csproj` para incluir el archivo de recursos `frmProductos.resx`, y se ha añadido la estructura XML correspondiente en este archivo para la gestión de recursos.

--- Forms/frmProductos.cs
+++ Forms/frmProductos.cs
@@ -1,12 +1,111 @@
+using MySql.Data.MySqlClient;
+using System;
+using System.Data;
 using System.Windows.Forms;
 
 namespace Punto.Forms
 {
     public partial class frmProductos : Form
     {
+        Conexion conexion = new Conexion();
+
         public frmProductos()
         {
             InitializeComponent();
+        }//Fin
+
+        //Categorias
+        private void frmProductos_Load(object sender, EventArgs e)
+        {
+            cmbCategorias.Items.Clear();
+
+            cmbCategorias.Items.Add("Bebidas");
+            cmbCategorias.Items.Add("Botanas");
+            cmbCategorias.Items.Add("Lácteos");
+            cmbCategorias.Items.Add("Abarrotes");
+            cmbCategorias.Items.Add("Limpieza");
+
+            CargarDatos();
         }
-    }
+
+        //Busqueda
+        private void txtBusqueda_TextChanged(object sender, EventArgs e)
+        {
+            CargarDatos(txtBusqueda.Text);
+        }
+
+        //Limpiar
+        private void Limpiar()
+        {
+            lblId.Text = "0";
+
+            txtCodigo.Clear();
+            txtNombre.Clear();
+            txtPrecio.Clear();
+            txtStock.Clear();
+
+            cmbCategorias.SelectedIndex = -1;
+        }
+
+        //Metodo
+        private void CargarDatos(string filtro = "")
+        {
+            try
+            {
+                using (MySqlConnection conn = conexion.GetConnection())
+                {
+                    string sql = @"SELECT * FROM productos WHERE descripcion LIKE @filtro OR codigo LIKE @filtro";
+
+                    MySqlCommand cmd = new MySqlCommand(sql, conn);
+
+                    cmd.Parameters.AddWithValue("@filtro", "%" + filtro + "%");
+
+                    MySqlDataAdapter da = new MySqlDataAdapter(cmd);
+
+                    DataTable dt = new DataTable();
+
+                    da.Fill(dt);
+
+                    dgvProductos.DataSource = dt;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        //Tabla 
+        private void dgvProductos_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex >= 0)
+            {
+                DataGridViewRow fila = dgvProductos.Rows[e.RowIndex];
+
+                lblId.Text = fila.Cells["producto_id"].Value.ToString();
+
+                txtCodigo.Text = fila.Cells["codigo"].Value.ToString();
+
+                txtNombre.Text = fila.Cells["descripcion"].Value.ToString();
+
+                txtPrecio.Text = fila.Cells["precio"].Value.ToString();
+
+                txtStock.Text = fila.Cells["stock"].Value.ToString();
+
+                cmbCategorias.Text = fila.Cells["categoria"].Value.ToString();
+            }
+        }
+
+        
+
+
+
+
+
+
+
+
+    }//Fin
+
+
 }
